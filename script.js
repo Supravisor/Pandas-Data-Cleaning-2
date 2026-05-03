9876728182af3b8b8d1173286fef130a83ce87c3
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-05-04 11:04:12 +1200

    add unique function

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 // Find unique values
 let variableUnique = document.getElementById("variableUnique");
 let columnUnique = document.getElementById("columnUnique");
+
+const unique = (arg) => {
+  if (variableUnique.value === "") {
+    return alert("Please enter a variable name in the 'variable' field, in the 'Find unique values' section.");
+  } else if (columnUnique.value === "") {
+    return alert("Please enter a column name in the 'column' field, in the 'Find unique values' section.");
+  } else {
+      document.editor.textbox.value+="\n" + variableUnique.value + "['" + columnUnique.value + "']." + arg;
+  }
+}
